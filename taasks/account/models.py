e666a759a94947ac1176b4d3fe6041395c9d7f43
Martynas Jocius <martynas@idiles.com>
2010-01-18 21:40:07 +0200

    Account models file added.

--- taasks/account/models.py
+++ taasks/account/models.py
@@ -2,6 +2,12 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class UserProfile(models.Model):
-    # user = models.ForeignKey(User, unique=True)
     full_name = models.CharField(max_length=100)
     
+    
+# def create_profile(signal, **kwargs):
+#     if kwargs.get('created'):
+#         user = kwargs.get('instance')
+#         profile, new = UserProfile.objects.get_or_create(user=user)
+# 
+# models.signals.post_save.connect(create_profile, sender=User)
