427c5333a2d3aa653fe580ac199569bd86cd083d
Martynas Jocius <martynas@idiles.com>
2010-01-19 14:32:18 +0200

    Account model migration for a photo field created.

---
+++ taasks/account/migrations/0004_add_photo.py
@@ -0,0 +1,69 @@
+
+from south.db import db
+from django.db import models
+from account.models import *
+
+class Migration:
+    
+    def forwards(self, orm):
+        
+        # Adding field 'UserProfile.photo'
+        db.add_column('account_userprofile', 'photo', orm['account.userprofile:photo'])
+        
+    
+    
+    def backwards(self, orm):
+        
+        # Deleting field 'UserProfile.photo'
+        db.delete_column('account_userprofile', 'photo')
+        
+    
+    
+    models = {
+        'account.userprofile': {
+            'about': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'experience': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'location': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True'}),
+            'photo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True'}),
+            'timezone': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'primary_key': 'True'}),
+            'website': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        'auth.group': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '80', 'unique': 'True'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'unique_together': "(('content_type', 'codename'),)"},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'max_length': '30', 'unique': 'True'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'unique_together': "(('app_label', 'model'),)", 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+    
+    complete_apps = ['account']
--- taasks/account/models.py
+++ taasks/account/models.py
@@ -1,8 +1,19 @@
+import os
+
 from django.contrib.auth.models import User
 from django.contrib import admin
 from django.db import models
 
 class UserProfile(models.Model):
+    
+    def make_photo_upload_path(instance, filename):
+        import pdb; pdb.set_trace()
+        ext = os.splitpath(filename)[1]
+        path = 'photos/%s%s' % (instance.user.id, ext)
+        print path
+        return path
+        
+    
     user = models.ForeignKey(User, primary_key=True)
     name = models.CharField(max_length=100, null=True)
     location = models.CharField(max_length=100, blank=True, null=True)
@@ -10,6 +21,7 @@ class UserProfile(models.Model):
     about = models.CharField(max_length=100, blank=True, null=True)
     experience = models.TextField(blank=True, null=True)
     timezone = models.CharField(max_length=50, blank=True, null=True)
+    photo = models.ImageField(upload_to=make_photo_upload_path, null=True)
     
 admin.site.register(UserProfile)
 
